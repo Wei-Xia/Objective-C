0f5df7eb04361372f2421e5ac816ee8240a4f747
James Hsia <xw900812@gmail.com>
2015-06-24 19:44:14 -0400

    Chapter 4

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Wei on 6/20/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    float a=3.2;
+    float b=42.4;
+    
+    double sum;
+    sum = a+b;
+    
+    printf("Sum is sum %f.\n", sum);
+    
+    return 0;
+}
+
