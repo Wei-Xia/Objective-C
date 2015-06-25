02b70b589a3f04ec5eebd97c4f6b3b213212c47b
James Hsia <xw900812@gmail.com>
2015-06-24 21:00:00 -0400

    Chapter 5

--- Degrees/Degrees/main.c
+++ Degrees/Degrees/main.c
@@ -8,11 +8,18 @@
 
 #include <stdio.h>
 
+float fahrenheitFromCelsius(float cel)
+{
+    float fahr= cel*1.8+32.0;
+    printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
+    return fahr;
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    float freezeINC=0;
+    float freezeINF=fahrenheitFromCelsius(freezeINC);
+    printf("Water freezes at %f degrees Fahrenheit.\n", freezeINF);
     return 0;
 }
 
