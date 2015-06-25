f1483aad687b6cc69a68af0ab1162fc7667a9263
James Hsia <xw900812@gmail.com>
2015-06-24 21:59:59 -0400

    Chapter 5

--- Degrees/Degrees/main.c
+++ Degrees/Degrees/main.c
@@ -9,8 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>   //way 2
 
+float lastTemperature;   //Global variables;
+
 float fahrenheitFromCelsius(float cel)
 {
+    lastTemperature = cel;
     float fahr= cel*1.8+32.0;
     printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
     return fahr;
@@ -18,9 +21,10 @@ float fahrenheitFromCelsius(float cel)
 
 int main(int argc, const char * argv[])
 {
-    float freezeINC=0;
+    float freezeINC=12.12;
     float freezeINF=fahrenheitFromCelsius(freezeINC);
     printf("Water freezes at %f degrees Fahrenheit.\n", freezeINF);
+    printf("The last temperature converted was %f. \n", lastTemperature);
     //return 0;
     return EXIT_SUCCESS;  //way 2
 }
