7d5610fa99aba66d5fcc73f7153c4c3da8edae7e
James Hsia <xw900812@gmail.com>
2015-06-24 22:02:41 -0400

    Chapter 5

---
+++ Triangle/Triangle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Triangle
+//
+//  Created by Wei on 6/24/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
