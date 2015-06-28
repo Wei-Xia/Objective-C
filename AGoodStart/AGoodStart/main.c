5524cfb0a18cf6ab2455ef21f6467bad1cd5f7bc
James Hsia <xw900812@gmail.com>
2015-06-27 22:51:09 -0400

    Chapter 6

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Wei on 6/27/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
