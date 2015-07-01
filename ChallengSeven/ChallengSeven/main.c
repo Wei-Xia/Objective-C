6e92ef3fd05b0f644c1fd476ea3f180025033b18
James Hsia <xw900812@gmail.com>
2015-06-30 23:16:26 -0400

    Chapter 7

--- ChallengSeven/ChallengSeven/main.c
+++ ChallengSeven/ChallengSeven/main.c
@@ -19,11 +19,14 @@ int main(int argc, const char * argv[])
     
     printf("the sine of %f radian is %.3f.\n", x, sin(x));
     
-
+    double radiantOf1 = cos(1); // Create a variable that use the "sin function" to calculate and store the result -> You can see that "x" is replace with number 1.
     
-    double radiantOf1 = sin(1); // Create a variable that use the "sin function" to calculate and store the result -> You can see that "x" is replace with number 1.
+    printf("the cosine of %f radian is %.3f.\n", x,radiantOf1); //Print out the result with 3 numbers after "."
     
-    printf("Your answer is: %.3f\n" ,radiantOf1); //Print out the result with 3 numbers after "."
+    double w=9;
+    double z=6;
+    
+    printf("%.0f/%.0f, the remainder is %.0f.\n", z,w,remainder(z,w));
     
     return 0;
 }
