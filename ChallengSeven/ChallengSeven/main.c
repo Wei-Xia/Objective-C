0c914bacec8785e47d1bbaa1f5141a2f80caf8be
James Hsia <xw900812@gmail.com>
2015-06-30 22:59:05 -0400

    Chapter 7

---
+++ ChallengSeven/ChallengSeven/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ChallengSeven
+//
+//  Created by Wei on 6/30/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
