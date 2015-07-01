1ad16014fdcac1389e6d0b93de814eb15a3270c4
James Hsia <xw900812@gmail.com>
2015-06-30 23:08:26 -0400

    Chapter 7

--- ChallengSeven/ChallengSeven/main.c
+++ ChallengSeven/ChallengSeven/main.c
@@ -8,11 +8,23 @@
 
 #include <stdio.h>
 
+#include <math.h> //use math in Mac, type 'man math' in Terminal
+
+//double sin(double x);   this is a function that takes one argument "(double x) "
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    double x=1;
+    
+    printf("the sine of %f radian is %.3f.\n", x, sin(x));
+    
+
+    
+    double radiantOf1 = sin(1); // Create a variable that use the "sin function" to calculate and store the result -> You can see that "x" is replace with number 1.
+    
+    printf("Your answer is: %.3f\n" ,radiantOf1); //Print out the result with 3 numbers after "."
+    
     return 0;
 }
 
