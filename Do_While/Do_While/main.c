e858f4cea86005682ff8bb82f01c99e09642f0e9
James Hsia <xw900812@gmail.com>
2015-07-01 21:53:35 -0400

    Chapter 8

---
+++ Do_While/Do_While/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Do_While
+//
+//  Created by Wei on 7/1/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i=1;
+    do{
+        printf("%d. Wei is so cool\n", i);
+        i++;
+    } while (i<=5);
+    
+    return 0;
+}
+
