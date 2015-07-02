76d884fd82e7d500253cf0a0ded5c5d4c6af0a3d
James Hsia <xw900812@gmail.com>
2015-07-01 21:30:59 -0400

    Chapter 8

---
+++ For_Loop/For_Loop/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  For_Loop
+//
+//  Created by Wei on 7/1/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
