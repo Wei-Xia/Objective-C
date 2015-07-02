9b514e34747a7f73e09d40c34889c536e1b23877
James Hsia <xw900812@gmail.com>
2015-07-01 22:15:59 -0400

    Chapter 8

---
+++ Challenge 8/Challenge 8/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Challenge 8
+//
+//  Created by Wei on 7/1/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i=19;
+    
+    do{
+        printf("%d.\n",i);
+        i=i-1;
+    }while(i==0);
+    
+    return 0;
+}
+
