dceeaf5753bb3312916ea9886591d51e13e133ac
James Hsia <xw900812@gmail.com>
2015-07-01 21:28:42 -0400

    Chapter 8

---
+++ While_Loop/While_Loop/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  While_Loop
+//
+//  Created by Wei on 7/1/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i=1;
+    
+    while (i<=5) {
+        printf("%d. Wei is cool.\n",i);
+        i++;
+    }
+    
+    return 0;
+}
+
