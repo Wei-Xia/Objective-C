b86f9faf4943e39de2f25c6984dc9598cb4eea79
James Hsia <xw900812@gmail.com>
2015-07-03 17:55:52 -0400

    Chapter 9

---
+++ Addresses/Addresses/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Addresses
+//
+//  Created by Wei on 7/3/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    int i=17;
+    printf("i stores its value at %p\n", &i);
+    printf("\n");
+    
+    int *addressofi=&i;
+    printf("i stores its value at %p\n",addressofi);
+    
+    printf("\n");
+    printf("This function starts at %p\n", main);
+    
+    printf("\n");
+    printf("The int stored at addressofi is %d\n", *addressofi);
+    
+    return 0;
+}
+
