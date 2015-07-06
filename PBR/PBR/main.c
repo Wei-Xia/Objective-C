552e4e7bc91a1f245d5738711e0fb96cd241a57a
James Hsia <xw900812@gmail.com>
2015-07-06 15:56:12 -0400

    Chapter 10

--- PBR/PBR/main.c
+++ PBR/PBR/main.c
@@ -9,11 +9,39 @@
 #include <stdio.h>
 #include <math.h>
 
+void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr)
+{
+    // This function assumes meters is non-negative.
+
+    // Convert the number of meters into a floating-point number of feet
+    double rawFeet = meters *3.281;
+
+    //How many complete feet as an unsigned int?
+    unsigned int feet = (unsigned int)floor(rawFeet);
+
+    // Store the number of feet at the supplied address
+    printf("Storing %u to the address %p\n", feet, ftPtr);
+    *ftPtr = feet;
+    
+    // Calculate inches
+    double fractionalFoot = rawFeet-feet;
+    double inches = fractionalFoot*12.0;
+    
+    // Store the number of inches at the supplied address
+    printf("Storing %.2f to the address %p\n", inches, inPtr);
+    *inPtr=inches;
+}
+
 int main(int argc, const char * argv[])
 {
+    double meters =3.0;
+    unsigned int feet;
+    double inches;
+    
+    metersToFeetAndInches(meters, &feet, &inches);
+    
+    printf("%.lf meters is equal to %d feet and %.1f inches.", meters, feet, inches);
 
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
 
