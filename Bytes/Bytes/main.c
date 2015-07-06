56f572d11b0f8c974adf15f1001ee875ef5e3c38
James Hsia <xw900812@gmail.com>
2015-07-06 12:27:55 -0400

    Chapter 9

---
+++ Bytes/Bytes/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  Bytes
+//
+//  Created by Wei on 7/6/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i=17;
+    int *addressOfI=&i;
+    printf("i stores its value at %p\n", addressOfI);
+    
+    *addressOfI=89;
+    printf("Now i is %d\n",i);
+    printf("An int is %zu bytes \n",sizeof(int));
+    printf("A pointer is %zu bytes \n", sizeof(int *));
+    
+    return 0;
+}
+
