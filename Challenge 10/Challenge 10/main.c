0e61ff6e6ebec2fdeac89cc3764af11e0e5636c7
James Hsia <xw900812@gmail.com>
2015-07-07 21:56:03 -0400

    Chapter 10

---
+++ Challenge 10/Challenge 10/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Challenge 10
+//
+//  Created by Wei on 7/7/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
