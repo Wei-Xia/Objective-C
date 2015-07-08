1a355326e282bfc20a2638db6d272e633b8d24d4
James Hsia <xw900812@gmail.com>
2015-07-07 22:04:26 -0400

    Chapter 11

---
+++ Struct/Struct/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  Struct
+//
+//  Created by Wei on 7/7/15.
+//  Copyright (c) 2015 OO. All rights reserved.
+//
+
+#include <stdio.h>
+
+// Here is the declaration of the struct
+
+struct Person {
+    float heightInMeters;
+    int weightInKilos;
+};
+
+
+int main(int argc, const char * argv[])
+{
+    struct Person mikey;
+    mikey.heightInMeters=1.7;
+    mikey.weightInKilos=96;
+    
+    struct Person aaron;
+    aaron.heightInMeters=1.97;
+    aaron.weightInKilos=84;
+    
+    printf("mikey is %.2f meters tall.\n", mikey.heightInMeters);
+    printf("mikey weights %d kilograms.\n\n", mikey.weightInKilos);
+    
+    printf("arron is %.2f meters tall.\n", aaron.heightInMeters);
+    printf("arron weights %d kilograms.\n", aaron.weightInKilos);
+    
+    return 0;
+}
+
