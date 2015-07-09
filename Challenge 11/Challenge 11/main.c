39d97b9d890791c2cb3c84c67056864cc3b20355
James Hsia <xw900812@gmail.com>
2015-07-08 22:04:38 -0400

    Chapter 11

--- Challenge 11/Challenge 11/main.c
+++ Challenge 11/Challenge 11/main.c
@@ -7,12 +7,19 @@
 //
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    long secondsSince1970=time(NULL);
+    
+    printf("It has been %ld seconds since 1970.\n",secondsSince1970);
+    
+    struct  tm  now;
+    localtime_r(&secondsSince1970, &now);
+    
+    printf("The time is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
+    
     return 0;
 }
 
