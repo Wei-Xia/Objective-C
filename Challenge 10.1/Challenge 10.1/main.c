b5bb23f020c3bec5f9275f8c6f1c365c628f0c72
James Hsia <xw900812@gmail.com>
2015-07-08 21:07:20 -0400

    Chapter 10

--- Challenge 10.1/Challenge 10.1/main.c
+++ Challenge 10.1/Challenge 10.1/main.c
@@ -11,25 +11,18 @@
 
 void metersToFeetAndInches(double meters,  int *ftPtr, double *inPtr)
 {
-    // This function assumes meters is non-negative.
-    
-    // Convert the number of meters into a floating-point number of feet
     double rawFeet = meters *3.281;
     
-    //How many complete feet as an unsigned int?
     int feet = (int)rawFeet;
     
-    // Store the number of feet at the supplied address
     if(ftPtr){
         printf("Storing %u to the address %p\n", feet, ftPtr);
         *ftPtr = feet;
     }
     
-    // Calculate inches
     double fractionalFoot = rawFeet-feet;
     double inches = fractionalFoot*12.0;
     
-    // Store the number of inches at the supplied address
     if (inPtr) {
         printf("Storing %.2f to the address %p\n", inches, inPtr);
         *inPtr=inches;
