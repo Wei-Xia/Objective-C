830fe7b089ea9cd306d31c19d75aecd958fba43a
James Hsia <xw900812@gmail.com>
2015-07-08 21:19:10 -0400

    Chapter 10

--- Challenge 10.1/Challenge 10.1/main.c
+++ Challenge 10.1/Challenge 10.1/main.c
@@ -13,27 +13,15 @@ void metersToFeetAndInches(double meters,  int *ftPtr, double *inPtr)
 {
     double rawFeet = meters *3.281;
     
-    int feet = (int)rawFeet;
+    double feet = (int)rawFeet;
     
-    if(ftPtr){
-        printf("Storing %u to the address %p\n", feet, ftPtr);
-        *ftPtr = feet;
-    }
-    
-    double fractionalFoot = rawFeet-feet;
-    double inches = fractionalFoot*12.0;
-    
-    if (inPtr) {
-        printf("Storing %.2f to the address %p\n", inches, inPtr);
-        *inPtr=inches;
-    }
-    
+    double inches = modf(rawFeet,&feet);
 }
 
 int main(int argc, const char * argv[])
 {
     double meters =3.0;
-    int feet;
+    double feet;
     double inches;
     
     metersToFeetAndInches(meters, &feet, &inches);
