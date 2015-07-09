1934e6c09b70f2e10ce83527477369cde00136f1
James Hsia <xw900812@gmail.com>
2015-07-08 20:49:59 -0400

    Chapter 10

--- Challenge 10/Challenge 10/main.c
+++ Challenge 10/Challenge 10/main.c
@@ -2,17 +2,27 @@
 //  main.c
 //  Challenge 10
 //
-//  Created by Wei on 7/7/15.
+//  Created by Wei on 7/8/15.
 //  Copyright (c) 2015 OO. All rights reserved.
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    double meter = 3.0;
+    unsigned int feet;
+    double inch;
+    
+    double rawFeet=meter*3.281;
+    
+    printf("%.2f", rawFeet);
+    
+    inch = modf(rawFeet,&feet);
+    
+    printf("%.2f meters equals to %d feet and %.3f inches.\n", meter, feet, inch);
+    
     return 0;
 }
 
