355550b651fde2a6c01c919b0938840dd2cc8bec
James Hsia <xw900812@gmail.com>
2015-07-09 22:05:54 -0400

    Chapter 15

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Wei on 7/9/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *currentTime1 = nil;  //this is nil for zero value
+        NSLog(@"CurrentTime's value is %p", currentTime1);
+        
+        NSDate *currentTime = [NSDate date];
+        NSLog(@"CurrentTime's value is %p", currentTime);
+        
+    }
+    return 0;
+}
+
