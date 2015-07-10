dee80501e39d38b937d8d886273775f810a45c8a
James Hsia <xw900812@gmail.com>
2015-07-09 22:46:14 -0400

    Chapter 14

---
+++ Challenge 14/Challenge 14/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Challenge 14
+//
+//  Created by Wei on 7/9/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
