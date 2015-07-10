21084d65fdbc8d2f7d98be397b6e4099908adf96
James Hsia <xw900812@gmail.com>
2015-07-10 15:40:29 -0400

    Chapter 14

--- Challenge 14/Challenge 14/main.m
+++ Challenge 14/Challenge 14/main.m
@@ -13,8 +13,20 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        
+        [comps setYear:1990];
+        [comps setMonth:8];
+        [comps setDay:12];
+        [comps setHour:13];
+        [comps setMinute:29];
+        [comps setSecond:32];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        
+        NSDate *laterDate
+        double secondsSinceEarlierDate = [laterDate timeIntervalsinceDate:dateOfBirth];
         
     }
     return 0;
