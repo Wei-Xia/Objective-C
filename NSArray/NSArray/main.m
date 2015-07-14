37a9ab06e77a496ef7a9b21740313503f3646371
James Hsia <xw900812@gmail.com>
2015-07-14 17:25:18 -0400

    Chapter 17

---
+++ NSArray/NSArray/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSArray
+//
+//  Created by Wei on 7/14/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
