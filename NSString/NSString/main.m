065f814bc51841bebc3f9d6a3e24beb124a470b9
James Hsia <xw900812@gmail.com>
2015-07-13 22:21:53 -0400

    Chapter 16

---
+++ NSString/NSString/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSString
+//
+//  Created by Wei on 7/13/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
