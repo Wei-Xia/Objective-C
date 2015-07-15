748498c1898fd896555f8c3e861b695a8979bf6f
James Hsia <xw900812@gmail.com>
2015-07-14 21:45:37 -0400

    Chapter 17

--- NSMutableArray/NSMutableArray/main.m
+++ NSMutableArray/NSMutableArray/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  NSMutableArray
+//  NSArray
 //
 //  Created by Wei on 7/14/15.
 //  Copyright (c) 2015 Wei Xia. All rights reserved.
@@ -10,12 +10,32 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create three NSDate objects
+        NSDate *now = [NSDate date];
+        NSDate *tomorrow = [now dateByAddingTimeInterval:24.0*60.0*60.0];
+        NSDate *yesterday = [now dateByAddingTimeInterval:-24.0*60.0*60.0];
         
+        // Create an empty mutable array
+        NSMutableArray *dateList = [NSMutableArray array];
+        
+        // Add two dates to the array
+        [dateList addObject:now];
+        [dateList addObject:tomorrow];
+        
+        // Add yesterday at the beginning of the list
+        [dateList insertObject:yesterday atIndex:0];
+        
+        // Iterate over the array
+        for (NSDate *d in dateList) {
+            NSLog(@"Here is a date: %@",d);
+        }
+        
+        // Remove yesterday
+        [dateList removeObjectAtIndex:0];
+        NSLog(@"Now the first date is %@", dateList[0]);
     }
     return 0;
 }
