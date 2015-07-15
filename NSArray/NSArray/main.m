880cea8fabed0d77777b6bbdca6ea853321230dd
James Hsia <xw900812@gmail.com>
2015-07-14 21:13:32 -0400

    Chapter 17

--- NSArray/NSArray/main.m
+++ NSArray/NSArray/main.m
@@ -13,9 +13,21 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create three NSDate objects
+        NSDate *now = [NSDate date];
+        NSDate *tomorrow = [now dateByAddingTimeInterval:24.0*60.0*60.0];
+        NSDate *yesterday = [now dateByAddingTimeInterval:-24.0*60.0*60.0];
         
+        // Create an array containing all three
+        NSArray *dateList = @[now, tomorrow, yesterday];
+        
+        // Print a couple of dates
+        NSLog(@"The first date is %@.",dateList[0]);
+        NSLog(@"The second date is %@.",dateList[1]);
+        NSLog(@"The third date is %@.",dateList[2]);
+        
+        // How many dates are in the array?
+        NSLog(@"There are %lu dates.", [dateList count]);
     }
     return 0;
 }
