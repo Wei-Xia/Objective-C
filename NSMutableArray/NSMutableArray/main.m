f13fd6113ce1ccd9516aa801a90ef18cf3f0c18d
James Hsia <xw900812@gmail.com>
2015-07-14 21:40:08 -0400

    Chapter 17

---
+++ NSMutableArray/NSMutableArray/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSMutableArray
+//
+//  Created by Wei on 7/14/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
