5a488f929b2011db6f1fcc5f375dec2d4ae64100
James Hsia <xw900812@gmail.com>
2015-07-14 21:28:13 -0400

    Chapter 17

--- NSArray_forLoop/NSArray_forLoop/main.m
+++ NSArray_forLoop/NSArray_forLoop/main.m
@@ -21,15 +21,12 @@ int main(int argc, const char * argv[])
         // Create an array containing all three
         NSArray *dateList = @[now, tomorrow, yesterday];
         
-        // Print a couple of dates
-        NSLog(@"The first date is %@.",dateList[0]);
-        NSLog(@"The second date is %@.",dateList[1]);
-        NSLog(@"The third date is %@.",dateList[2]);
-        
-        // How many dates are in the array?
-        NSLog(@"There are %lu dates.", [dateList count]);
-        
-        NSLog(@"The third date is %@.",dateList[4]);  //Crash due to out-of-range
+        // Iterate over the array
+        NSUInteger dateCount = [dateList count];
+        for (int i =0; i<dateCount; i++) {
+            NSDate *d = dateList[i];
+            NSLog(@"Here is a date: %@", d);
+        }
     }
     return 0;
 }
