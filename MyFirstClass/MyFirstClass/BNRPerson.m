06d4ada5f9f27228465298012ed23fcf28b80af5
James Hsia <xw900812@gmail.com>
2015-07-15 21:53:50 -0400

    Chapter 18

--- MyFirstClass/MyFirstClass/BNRPerson.m
+++ MyFirstClass/MyFirstClass/BNRPerson.m
@@ -9,5 +9,30 @@
 #import "BNRPerson.h"
 
 @implementation BNRPerson
+- (float)heightInMeters
+{
+    return _heightInMeters;
+}
+
+- (void)setHeightInMeters:(float)h
+{
+    _heightInMeters = h;
+}
+
+- (int)weightInKilos;
+{
+    return _weightInKilos;
+}
+
+- (void)setWeightInKilos:(int)w
+{
+    _weightInKilos = w;
+}
+
+- (float)bodyMassIndex
+{
+    return _weightInKilos/(_weightInKilos*_heightInMeters);
+}
+
 
 @end
