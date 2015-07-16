3c57cd91a6d39ac088027feed65a25f34723ae0d
James Hsia <xw900812@gmail.com>
2015-07-15 22:09:42 -0400

    Chapter 18

--- MyFirstClass/MyFirstClass/main.m
+++ MyFirstClass/MyFirstClass/main.m
@@ -12,7 +12,25 @@
 
 int main(int argc, const char * argv[])
 {
-
+    @autoreleasepool {
+        
+    // Create an instance of BNRPerson
+    BNRPerson *mikey = [[BNRPerson alloc] init];
+    
+    // Give the instance variables interesting values using setters
+    [mikey setWeightInKilos:96];
+    [mikey setHeightInMeters:1.8];
+    
+    // Log the instance varibales using the getters
+    float height = [mikey heightInMeters];
+    int weight = [mikey weightInKilos];
+    NSLog(@"mikey is %.2f meters tall and weights %d kilograms", height, weight);
+    
+    // Log some values using custom methods
+    float bmi = [mikey bodyMassIndex];
+    NSLog(@"mikey has a BMI of %.3f", bmi);
+    
+    }
     return 0;
 }
 
