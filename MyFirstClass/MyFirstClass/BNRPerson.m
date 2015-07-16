45ed5b7f16631a6a40eb8590c72d00b2a0cba407
James Hsia <xw900812@gmail.com>
2015-07-15 21:33:29 -0400

    Chapter 18

---
+++ MyFirstClass/MyFirstClass/BNRPerson.m
@@ -0,0 +1,13 @@
+//
+//  BNRPerson.m
+//  MyFirstClass
+//
+//  Created by Wei on 7/15/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import "BNRPerson.h"
+
+@implementation BNRPerson
+
+@end
