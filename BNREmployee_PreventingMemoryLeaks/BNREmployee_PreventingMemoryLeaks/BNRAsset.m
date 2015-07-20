119483880c19806694b8702e8d71c4734f4ea9d1
James Hsia <xw900812@gmail.com>
2015-07-20 16:16:12 -0400

    Chapter 23

--- BNREmployee_PreventingMemoryLeaks/BNREmployee_PreventingMemoryLeaks/BNRAsset.m
+++ BNREmployee_PreventingMemoryLeaks/BNREmployee_PreventingMemoryLeaks/BNRAsset.m
@@ -13,7 +13,12 @@
 
 - (NSString *) description
 {
-    return [NSString stringWithFormat:@"<%@: $%d>", self.label, self.resaleValue];
+    // Is holder non-nil?
+    if (self.holder) {
+        return [NSString stringWithFormat:@"<%@: $%d, assigned to %@>", self.label, self.resaleValue, self.holder];
+    } else{
+        return [NSString stringWithFormat:@"<%@: $%d unassigned>", self.label, self.resaleValue];
+    }
 }
 
 - (void) dealloc
