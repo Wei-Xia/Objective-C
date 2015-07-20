31c97c66fb6f8e49460399e18003382bb64002bd
James Hsia <xw900812@gmail.com>
2015-07-20 11:00:10 -0400

    Chapter 21

--- BNREmployee_Object/BNREmployee_Object/BNREmployee.m
+++ BNREmployee_Object/BNREmployee_Object/BNREmployee.m
@@ -7,33 +7,9 @@
 //
 
 #import "BNREmployee.h"
+#import "BNRAsset.h"
 
 @implementation BNREmployee
 
--(double) yearOfEmployment
-{
-    // Do i have a non-nil hireDate?
-    if (self.hireDate) {
-        //NSTimeInterval is the same as double
-        NSDate *now = [NSDate date];
-        NSTimeInterval secs = [now timeIntervalSinceDate:self.hireDate];
-        return secs/31557600.0;   //Seconds per year
-    }
-    else {return 0;}
-}
-
-
-// Overriding methods
-- (float) bodyMassIndex
-{
-    //return 19.0;
-    float normalBMI=[super bodyMassIndex];
-    return normalBMI*0.9;
-}
-
-- (NSString *) description
-{
-    return [NSString stringWithFormat:@"<Employee %d>", self.employeeID];
-}
 
 @end
