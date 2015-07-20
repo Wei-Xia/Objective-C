a6f29faee5f56862b4e84b9234b46bf040130305
James Hsia <xw900812@gmail.com>
2015-07-20 14:38:53 -0400

    Chapter 22

---
+++ BNREmployee_ClassExtension/BNREmployee_ClassExtension/BNREmployee_officeAlarmCode.h
@@ -0,0 +1,17 @@
+//
+//  BNREmployee_officeAlarmCode.h
+//  BNREmployee_ClassExtension
+//
+//  Created by Wei on 7/20/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import "BNREmployee.h"
+
+// A class extension
+
+@interface BNREmployee ()
+
+@property (nonatomic) unsigned int officeAlarmCode;
+
+@end
