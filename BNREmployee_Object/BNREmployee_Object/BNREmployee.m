36852487e572463acfbc0d853a871f74f11ede19
James Hsia <xw900812@gmail.com>
2015-07-20 11:09:03 -0400

    Chapter 21

--- BNREmployee_Object/BNREmployee_Object/BNREmployee.m
+++ BNREmployee_Object/BNREmployee_Object/BNREmployee.m
@@ -11,5 +11,35 @@
 
 @implementation BNREmployee
 
+// Accessors for assets properties
+- (void)setAssets:(NSArray *)a
+{
+    _assets = [a mutableCopy];
+}
+
+- (NSArray *)assets
+{
+    return [_assets copy];
+}
+
+- (void)addAsset:(BNRAsset *)a
+{
+    //Is assets nil?
+    if (!_assets){
+        // Create the array
+        _assets = [[NSMutableArray alloc] init];
+    }
+    [_assets addObject:a];
+}
+
+- (unsigned int) valueOfAssets
+{
+    // Sum up the resale value of the assets
+    unsigned int sum = 0;
+    for (BNRAsset *a in _assets) {
+        sum = sum+[a resaleValue];
+    }
+    return sum;
+}
 
 @end
