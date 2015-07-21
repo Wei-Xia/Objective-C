2dd6196b69fe6314e4d4d23a488f661f71f4e07c
James Hsia <xw900812@gmail.com>
2015-07-21 17:42:31 -0400

    Chapter 25

---
+++ Constants/Constants/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by Wei on 7/21/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSLog(@"\u03c0 is %f", M_PI);
+    }
+    return 0;
+}
+
