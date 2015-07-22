c0e0bb629b938633a952e742a68775a2823261ba
James Hsia <xw900812@gmail.com>
2015-07-21 21:25:35 -0400

    Chapter 25

--- Constants/Constants/main.m
+++ Constants/Constants/main.m
@@ -15,6 +15,13 @@ int main(int argc, const char * argv[])
         
         NSLog(@"\u03c0 is %f", M_PI);
         NSLog(@"%d is larger", MAX(10, 12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        
+//      NSString *currency = [here objectForKey:@"currency"];   // Way one
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];    // Way Two
+        
+        NSLog(@"Money is %@", currency);
     }
     return 0;
 }
