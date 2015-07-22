8caaf53be65f35c9cd145754375eca0d72f0aab8
James Hsia <xw900812@gmail.com>
2015-07-21 22:41:17 -0400

    Chapter 26

---
+++ Stringz/Stringz/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by Wei on 7/21/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSMutableString *str = [[NSMutableString alloc] init];
+        
+        for (int i=0; i<10; i++) {
+            [str appendString:@"Aaron is cool! n\"];
+        }
+        [str writeToFile:@"/tmp/cool.txt"
+              atomically:YES
+                encoding:NSUTF8StringEncoding
+                   error:NULL];
+             NSLog(@"done writing /tmp/cool.txt");
+        
+    }
+    return 0;
+}
+
