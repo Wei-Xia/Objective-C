d2c15691471f97d5b89899febfadc577b2f4b561
James Hsia <xw900812@gmail.com>
2015-07-22 22:55:31 -0400

    Chapter 27

---
+++ Callbacks/Callbacks/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Wei on 7/22/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        [[NSRunLoop currentRunLoop] run];
+        
+    }
+    return 0;
+}
+
