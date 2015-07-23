e8144e7c2356bb4dfe5ecfa9515b57ea7fe845cd
James Hsia <xw900812@gmail.com>
2015-07-23 09:57:16 -0400

    Chapter 27

---
+++ Callbacks/Callbacks/BNRLogger.h
@@ -0,0 +1,17 @@
+//
+//  BNRLogger.h
+//  Callbacks
+//
+//  Created by Wei on 7/23/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRLogger : NSObject
+
+@property (nonatomic) NSDate *lastTime;
+- (NSString *)lastTimeString;
+- (void)updateLastTime: (NSTimer *)t;
+
+@end
