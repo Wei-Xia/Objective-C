e61d387035d57e2cb1fe25a7522b3eceb69c74ad
James Hsia <xw900812@gmail.com>
2015-07-23 10:06:23 -0400

    Chapter 27

--- Callbacks/Callbacks/main.m
+++ Callbacks/Callbacks/main.m
@@ -7,11 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRLogger.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        
+        BNRLogger *logger = [[BNRLogger alloc]init];
+        
+        NSTimer *timer= [NSTimer scheduledTimerWithTimeInterval:2.0
+                                                         target:logger
+                                                       selector:@selector(updateLastTime:)
+                                                       userInfo:nil
+                                                        repeats:YES];
         
         [[NSRunLoop currentRunLoop] run];
         
