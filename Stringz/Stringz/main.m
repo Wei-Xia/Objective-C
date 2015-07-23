4ac57c84f9ec4dde705e812de2251043705ea196
James Hsia <xw900812@gmail.com>
2015-07-22 21:53:59 -0400

    Chapter 26

--- Stringz/Stringz/main.m
+++ Stringz/Stringz/main.m
@@ -18,11 +18,29 @@ int main(int argc, const char * argv[])
         for (int i=0; i<10; i++) {
             [str appendString:@"Aaron is cool! \n"];
         }
+        
+        /*
         [str writeToFile:@"/tmp/cool.txt"
               atomically:YES
                 encoding:NSUTF8StringEncoding
                    error:NULL];
              NSLog(@"done writing /desktop/cool.txt");   // Use Find - Go, enter /Volumes/Macintosh HD/private/tmp to find the file
+        */
+        // Declare a pointer to an NSError object, but don't instantiate it.
+        // The NSError instance will only be created if there is, in fact, an error.  NSError *error;
+        
+        // Pass the NSError pointer by reference to the NSString method
+        BOOL success = [str writeToFile:@"/tmp/cool.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&error];
+        
+        // Test the returned BOOL, and query the NSError if the write failed
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
         
     }
     return 0;
