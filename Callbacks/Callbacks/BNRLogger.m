e85faa1779ffd1e44cf1fb3e4db44885a920d6f5
James Hsia <xw900812@gmail.com>
2015-07-24 12:00:55 -0400

    Chapter 27

--- Callbacks/Callbacks/BNRLogger.m
+++ Callbacks/Callbacks/BNRLogger.m
@@ -10,7 +10,42 @@
 
 @implementation BNRLogger
 
+// Called each time a chunk of data arrives
+- (void)connection: (NSURLConnection *) connection
+    didReceiveData:(NSData *)data
+{
+    NSLog(@"received %lu bytes", [data length]);
+    
+    // Create a mutable data if it doesn't already exist.
+    if (!incomingData) {
+        incomingData = [[NSMutableData alloc]init];
+    }
+    
+    [incomingData appendData:data];
+    
+}
 
+// Called when the last chunk has been processed
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"Got it all!");
+    NSString *string = [[NSString alloc] initWithData:incomingData
+                                             encoding:NSUTF8StringEncoding];
+    
+    incomingData = nil;
+    NSLog(@"string has %lu characters", [string length]);
+    
+    // Uncomment the next line to see the entire fetched file
+    NSLog(@"The whole string is %@", string);
+}
+
+// Called if the fetch fails
+- (void)connection:(NSURLConnection *)connection
+    didFailWithError:(NSError *)error
+{
+    NSLog(@"connection failed %@", [error localizedDescription]);
+    incomingData=nil;
+}
 
 - (NSString *)lastTimeString
 {
