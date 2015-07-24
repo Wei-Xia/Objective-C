a66214857c5060ff8efae02db22ade856e92a409
James Hsia <xw900812@gmail.com>
2015-07-24 13:52:29 -0400

    Chapter 28

---
+++ VowelMovement/VowelMovement/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Wei on 7/24/15.
+//  Copyright (c) 2015 Wei Xia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // Create array of string and a container for devowelized ones
+        NSArray *originalStrings = @[@"Sauerkraut", @"Raygun", @"Big Nerd Ranch", @"Mississippi"];
+        
+        NSLog(@"Original strings: %@", originalStrings);
+        
+        NSMutableArray *devowelizedStrings = [NSMutableArray array];
+        
+        // Create a list of characters to be removed from the string
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+    }
+    return 0;
+}
+
