047b2fced4aae575b7b61bf83ee94bd7ecaa5628
Wei Xia <xw900812@gmail.com>
2015-07-26 23:42:51 -0400

    Chapter 30

---
+++ Stockz/Stockz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Wei on 7/26/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
