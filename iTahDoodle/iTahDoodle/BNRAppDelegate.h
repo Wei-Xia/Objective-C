c8bda3ed560cd41c06b0309bc6e2966c6bbbd2e1
Wei Xia <xw900812@gmail.com>
2015-07-27 11:56:54 -0400

    Chapter 31

---
+++ iTahDoodle/iTahDoodle/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  iTahDoodle
+//
+//  Created by Wei on 7/27/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTahDoodle/iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by Wei on 7/27/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
