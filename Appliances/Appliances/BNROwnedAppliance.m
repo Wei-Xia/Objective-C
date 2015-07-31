9ea547b54060be106276a2791021395d3bc07845
Wei Xia <xw900812@gmail.com>
2015-07-30 23:22:40 -0400

    Chapter 33

--- Appliances/Appliances/BNROwnedAppliance.m
+++ Appliances/Appliances/BNROwnedAppliance.m
@@ -8,6 +8,34 @@
 
 #import "BNROwnedAppliance.h"
 
+@interface  BNROwnedAppliance()
+{
+    NSMutableSet *_ownerNames;
+}
+
+@end
+
 @implementation BNROwnedAppliance
+
+-(instancetype)initWithProductName:(NSString *)pn firstOwnerName:(NSString *)n
+{
+    // Call the superclass's initalizer
+    if (self = [super initWithProductName:pn]) {
+        
+        // Create a set to hold owners names
+        _ownerNames = [[NSMutableSet alloc] init];
+        
+        // Is the first owner name non-nil?
+        if (n) {
+            [_ownerNames addObject:n];
+        }
+    }
+    
+    // Return a pointer to the new object
+    return self;
+}
+
+
+
 
 @end
