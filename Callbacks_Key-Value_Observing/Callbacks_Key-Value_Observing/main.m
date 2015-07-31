9e23cd1b57da9e8a8109a95079832c82fd14ff2b
Wei Xia <xw900812@gmail.com>
2015-07-31 11:49:55 -0400

    Chapter 36

--- Callbacks_Key-Value_Observing/Callbacks_Key-Value_Observing/main.m
+++ Callbacks_Key-Value_Observing/Callbacks_Key-Value_Observing/main.m
@@ -18,20 +18,6 @@ int main(int argc, const char * argv[])
         
         BNRLogger *logger = [[BNRLogger alloc]init];
         
-        [[NSNotificationCenter defaultCenter] addObserver:logger
-                                                 selector:@selector(zoneChange:)
-                                                     name:NSSystemTimeZoneDidChangeNotification
-                                                   object:nil];
-        
-        NSURL *url = [NSURL URLWithString:@"http://www.gutenberg.org/cache/epub/205/pg205.txt"];
-        
-        NSURLRequest *request = [NSURLRequest requestWithURL:url];
-        
-        __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request
-                                                                              delegate:logger
-                                                                      startImmediately:YES];
-    
-        
         __unused NSTimer *timers = [NSTimer scheduledTimerWithTimeInterval:2.0     //silence the warning with __unused modifier
                                                                     target:logger
                                                                   selector:@selector(updateLastTime:)
