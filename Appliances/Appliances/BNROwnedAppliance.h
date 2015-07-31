d059767bc70c7cafff2b028dbf11856972e77d0d
Wei Xia <xw900812@gmail.com>
2015-07-30 23:16:44 -0400

    Chapter 33

---
+++ Appliances/Appliances/BNROwnedAppliance.h
@@ -0,0 +1,16 @@
+//
+//  BNROwnedAppliance.h
+//  Appliances
+//
+//  Created by Wei on 7/30/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRAppliance.h"
+
+@interface BNROwnedAppliance : BNRAppliance
+
+@
+
+@end
---
+++ Appliances/Appliances/BNROwnedAppliance.m
@@ -0,0 +1,13 @@
+//
+//  BNROwnedAppliance.m
+//  Appliances
+//
+//  Created by Wei on 7/30/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import "BNROwnedAppliance.h"
+
+@implementation BNROwnedAppliance
+
+@end
