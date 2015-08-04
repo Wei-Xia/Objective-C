8b42875e21f370b46202b84b77d14fbf62b02269
Wei Xia <xw900812@gmail.com>
2015-08-03 20:59:59 -0400

    Chapter 37

--- VowelCounter/VowelCounter/NSString+BNRVowelCounting.m
+++ VowelCounter/VowelCounter/NSString+BNRVowelCounting.m
@@ -10,4 +10,20 @@
 
 @implementation NSString (BNRVowelCounting)
 
+-(int)bnr_vowelCount
+{
+    NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString:@"aeiouyAEIOUY"];
+    
+    NSUInteger count = [self length];
+    int sum=0;
+    for (int=0; i<count; i++) {
+        unichar c = [self characterAtIndex:i];
+        if ([charSet characterIsMember:c]) {
+            sum++;
+        }
+    }
+    
+    return sum;
+}
+
 @end
