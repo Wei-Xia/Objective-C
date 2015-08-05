fe582a66fdf48e644d715192f324a468829779e4
Wei Xia <xw900812@gmail.com>
2015-08-04 23:17:43 -0400

    Chapter 39

--- Floating_Point_Numbers/Floating_Point_Numbers/main.c
+++ Floating_Point_Numbers/Floating_Point_Numbers/main.c
@@ -6,13 +6,29 @@
 //  Copyright (c) 2015 Gate Inc. All rights reserved.
 //
 
-#include <stdio.h>
+#include <stdio.h>  // For printf
+#include <stdlib.h>  // For malloc/free
+#include <string.h>  // For strlen
 
 int main(int argc, const char * argv[])
 {
+    // Claim a chunk of memory big enough to hold three floats
+    float *favorites = malloc(3 * sizeof(float));
+    
+    // Push values into the locations in that buffer
+    favorites[0] = 3.14158;
+    favorites[1] = 2.71828;
+    favorites[2] = 1.41421;
+    
+    // Print out each number on the list
+    for (int i = 0 ; i<3; i++) {
+        printf("%.4f is favorite %d\n", favorites[i], i+1);
+    }
+    
+    // Free the memory so that it can be reused
+    free(favorites);
+    favorites = NULL;
 
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
 
