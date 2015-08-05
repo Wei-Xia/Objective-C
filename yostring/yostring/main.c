01a7a5afbc40fc5572b767dd8cc91ad2ef82dc0d
Wei Xia <xw900812@gmail.com>
2015-08-04 22:17:36 -0400

    Chapter 39

---
+++ yostring/yostring/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  yostring
+//
+//  Created by Wei on 8/4/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
