d9e0918b42c20c797893533788b5e7b351646e1f
Wei Xia <xw900812@gmail.com>
2015-08-04 22:52:55 -0400

    Chapter 39

---
+++ C_String_char*/C_String_char*/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  C_String_char*
+//
+//  Created by Wei on 8/4/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#include <stdio.h>  // For printf
+#include <stdlib.h>  // For malloc/free
+#include <string.h>  // For strlen
+
+int main(int argc, const char * argv[])
+{
+    // Get a pointer to 5 bytes of memory on the heap
+    char
+    
+    
+    return 0;
+}
+
