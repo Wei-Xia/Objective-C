50d8592be72f6bd89488cd80833fa43a141d039c
Wei Xia <xw900812@gmail.com>
2015-08-06 12:05:46 -0400

    Chapter 41

--- Affirmation/Affirmation/main.c
+++ Affirmation/Affirmation/main.c
@@ -12,7 +12,7 @@ int main(int argc, const char * argv[])
 {
 
     for (int i=0; i<argc; i++) {
-        printf("arg %d = %s\n", i, argc[i]);
+        printf("arg %d = %s\n", i, argv[i]);
     }
     
     return 0;
