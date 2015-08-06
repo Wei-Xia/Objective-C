1fda7e50826ebc1a08c1da6f0c5e4b713462e765
Wei Xia <xw900812@gmail.com>
2015-08-06 11:54:41 -0400

    Chapter 41

---
+++ Affirmation/Affirmation/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Affirmation
+//
+//  Created by Wei on 8/6/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
